554b7b5955c2cb7400bc6e029c3d8a5cc75133c0
Kaochar <94604055+M-Khalekuzzaman@users.noreply.github.com>
2021-11-22 12:22:13 +0600

    Add files via upload

---
+++ Reverse caracter.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+   while(1)
+   {
+        char ch1,ch2,ch3;
+    cout<<"Enter 3 character are :";
+    cin>>ch1>>ch2>>ch3;
+    cout<<"Input 3 character are :" <<ch1<<ch2<<ch3<<endl;
+    cout<<"Reverse 3 character are : "<<ch3<<ch2<<ch1<<endl;
+  cout<<endl<<endl;
+   }
+
+    getch();
+}
